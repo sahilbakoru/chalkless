23ce4529b9e092b357fefcff5419088d1cc244cb
sahilbakoru <sahilbakoru1999@gmail.com>
2025-06-04 09:25:48 +0530

    some added feateures and redme update

--- README.md
+++ README.md
@@ -1,7 +1,50 @@
-# chalkless
+# Chalkless – (like Chalk, but no deps)
 
-Tiny utility to style your `console.log` messages without dependencies. Works in both Node.js and browser.
+Simple, zero-dependency console styling utility for Node.js and browser.
+
+## Features
+
+- Log colored text easily: `console.red('text')`
+- Styles like bold, italic, underline: `console.bold('text')`
+- Combined styles & colors: `console.boldRed('text')`
+- Works in both terminal and browser consoles
+- No dependencies, tiny and lightweight
+
+## Available Colors
+
+- black, red, green, yellow, blue, magenta, cyan, white
+- gray, lightGray
+- darkRed, darkGreen, darkYellow, darkBlue, darkMagenta, darkCyan
+- brightBlack, brightRed, brightGreen, brightYellow, brightBlue, brightMagenta, brightCyan, brightWhite
+
+## Available Styles
+
+- bold, italic, underline
+
+
+##  Installation
+```bash
+npm install chalkless
+```
+
+
+## Usage
 
 ```js
-const colorLog = require('chalkless');
-colorLog('Hello styled world!');
+require('chalkless');
+
+// Simple colors
+console.red('This is red text');
+console.green('This is green text');
+
+// Styles
+console.bold('Bold text');
+console.italic('Italic text');
+console.underline('Underlined text');
+
+// Combined styles and colors
+console.boldRed('Bold red text');
+console.italicGreen('Italic green text');
+console.underlineBrightBlue('Underlined bright blue text');
+
+```
--- index.js
+++ index.js
@@ -1,6 +1,56 @@
-// index.js
-function colorLog(msg) {
-  console.log('%c' + msg, 'color: blue; font-weight: bold;');
+// chalkless/index.js
+
+const COLORS = {
+  black: '\x1b[30m',
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[34m',
+  magenta: '\x1b[35m',
+  cyan: '\x1b[36m',
+  white: '\x1b[37m',
+
+  gray: '\x1b[90m',
+  lightGray: '\x1b[37m',
+
+  darkRed: '\x1b[31m',
+  darkGreen: '\x1b[32m',
+  darkYellow: '\x1b[33m',
+  darkBlue: '\x1b[34m',
+  darkMagenta: '\x1b[35m',
+  darkCyan: '\x1b[36m',
+};
+
+
+const STYLES = {
+  bold: { open: '\x1b[1m', close: '\x1b[22m' },
+  italic: { open: '\x1b[3m', close: '\x1b[23m' },
+  underline: { open: '\x1b[4m', close: '\x1b[24m' },
+};
+
+const RESET = '\x1b[0m';
+
+function styleLog(openCodes, closeCodes, ...args) {
+  console.log(openCodes + args.join(' ') + closeCodes);
 }
 
-module.exports = colorLog;
+// Add single color methods, e.g. console.red('text')
+for (const [colorName, colorCode] of Object.entries(COLORS)) {
+  console[colorName] = (...args) => styleLog(colorCode, RESET, ...args);
+}
+
+// Add single style methods, e.g. console.bold('text')
+for (const [styleName, codes] of Object.entries(STYLES)) {
+  console[styleName] = (...args) => styleLog(codes.open, codes.close + RESET, ...args);
+}
+
+// Add combined styles + colors, e.g. console.boldRed('text')
+for (const [styleName, styleCodes] of Object.entries(STYLES)) {
+  for (const [colorName, colorCode] of Object.entries(COLORS)) {
+    const methodName = styleName + colorName.charAt(0).toUpperCase() + colorName.slice(1);
+    console[methodName] = (...args) =>
+      styleLog(styleCodes.open + colorCode, styleCodes.close + RESET, ...args);
+  }
+}
+
+module.exports = {};
